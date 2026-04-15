d35be1573c1f91e31cba895aa179ef34ae10aa04
chezzi <chezzi@sina.cn>
2026-04-15 17:58:21 +0800

    Add GitHub Actions deployment for Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // 如果你的网站将部署在 https://chezzi.github.io/ 下，site 可以设置为 'https://chezzi.github.io'
-  // 如果部署在子路径（如 https://chezzi.github.io/my-repo/），则需要设置 base: '/my-repo'
-  site: 'https://chezzi.github.io',
-  // base: '/你的仓库名',   // 如果是用户/组织 Pages（username.github.io），则不需要 base
+ site:'https://chezzi.github.io',
+ # // 如果你的网站将部署在 https://chezzi.github.io/ 下，site 可以设置为 'https://chezzi.github.io'
+ # // 如果部署在子路径（如 https://chezzi.github.io/my-repo/），则需要设置 base: '/my-repo'
+ # site: 'https://chezzi.github.io',
+ # // base: '/你的仓库名',   // 如果是用户/组织 Pages（username.github.io），则不需要 base
 });
